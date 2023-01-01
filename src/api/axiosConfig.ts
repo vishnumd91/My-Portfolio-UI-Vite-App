3668ca7ca3d5419e6409939eda6c6c44487cb57e
Vishnu MD <vishnudivakaran1@gmail.com>
2023-01-01 20:09:38 +0530

    Fix for environment variables usage

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BASE_APP_URL}`,
+  baseURL: `${process.env.VITE_BASE_APP_URL}`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
--- src/services/experience-service.ts
+++ src/services/experience-service.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../api/axiosConfig";
 
 export const getExperienceDataFromSanity = async () => {
   const { data } = await axiosInstance.get(
-    `data/query/production?query=${import.meta.env.VITE_SANITY_QUERY}`
+    `data/query/production?query=${process.env.VITE_SANITY_QUERY}`
   );
   return data?.result;
 };
