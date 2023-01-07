3c7ea61e02cf18251a50c0a92640d0bcedcf9721
Vishnu MD <vishnudivakaran1@gmail.com>
2023-01-07 11:26:58 +0530

    Mapped about data with UI

--- src/services/web-services.ts
+++ src/services/web-services.ts
@@ -1,8 +1,8 @@
 import axiosInstance from "../api/axiosConfig";
 
-export const getExperienceDataFromSanity = async () => {
+export const getDataFromSanity = async (sanityQuery: string) => {
   const { data } = await axiosInstance.get(
-    `data/query/production?query=${process.env.VITE_SANITY_QUERY}`
+    `data/query/production?query=${sanityQuery}`
   );
   return data?.result;
 };
