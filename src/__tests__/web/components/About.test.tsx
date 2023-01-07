505bd2ffd91af5ed5fe07d50ac54133a3e993b45
Vishnu MD <vishnudivakaran1@gmail.com>
2023-01-07 11:37:54 +0530

    Test case fix

--- src/__tests__/web/components/About.test.tsx
+++ src/__tests__/web/components/About.test.tsx
@@ -1,10 +1,19 @@
 import { render } from "@testing-library/react";
-import React from "react";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "../../../utils";
 import { About } from "../../../web/components/about";
+
+const Mock = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <About />
+    </QueryClientProvider>
+  );
+};
 
 describe("About Test Cases", () => {
   it("should render the About Component", () => {
-    const val = render(<About />);
+    const val = render(<Mock />);
     expect(val).toBeTruthy();
   });
 });
